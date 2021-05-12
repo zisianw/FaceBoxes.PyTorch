ce6d333757b3978cdacd004d6c6989d10c98c525
shirakia <atsuo.shiraki@gmail.com>
2021-05-12 11:55:22 +0900

    setup.pyを追加

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="faceboxes_pytorch",
+    version="0.1.0",
+    author="Example Author",
+    author_email="author@example.com",
+    description="",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Liquid-dev/FaceBoxes.PyTorch",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    packages=setuptools.find_packages(),
+    python_requires=">=3.7",
+)
